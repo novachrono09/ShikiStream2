fdc9b026defada7b2486befeb685ab74e02f96bc
hn674250 <hn674250@gmail.com>
2025-11-20 04:53:31 +0000

    fix(hianime): correct scraper initialization

--- src/lib/hianime.ts
+++ src/lib/hianime.ts
@@ -2,4 +2,4 @@ import { HiAnime } from "aniwatch";
 
 const proxyUrl = process.env.NEXT_PUBLIC_PROXY_URL;
 
-export const hianime = new HiAnime.Scraper(proxyUrl);
+export const hianime = new HiAnime.Scraper();
